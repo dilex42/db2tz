796fd66bd5ef222d2f9da69c4fb6f98d1667e62d
Dima Alexeenko <dilex.banana@gmail.com>
2021-03-29 17:09:20 +0300

    clean urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,16 +1,8 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path
 from .views import MessageSingle, MessageCreate, MessageList
 
-router = routers.DefaultRouter()
-# router.register(r'list', MessageViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
     path("list/<int:page>/", MessageList.as_view()),
     path("create/", MessageCreate.as_view()),
     path("single/<int:pk>/", MessageSingle.as_view()),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
