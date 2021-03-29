f6951c9fcf7da22116524260716ea2af71c6d818
Dima Alexeenko <dilex.banana@gmail.com>
2021-03-29 16:55:18 +0300

    whnoise fix

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
